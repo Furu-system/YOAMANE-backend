eb44b8f253219eed3045b0c0bec3fe224c3d4ca2
Furu-system <mi7206furu@g.kumamoto-nct.ac.jp>
2021-09-01 13:35:28 +0000

    serializerで上書き(コメントアウト中)

--- app/serializer.py
+++ app/serializer.py
@@ -24,52 +24,78 @@ class UserSerializer(serializers.ModelSerializer):
         instance.save()
         return instance
 
+class GroupTagSerializer(serializers.ModelSerializer):
+    # user_id = UserSerializer()
+
+    class Meta:
+        model = GroupTags
+        fields = ('name', 'user_id')
+
 class ScheduleSerializer(serializers.ModelSerializer):
+    # user_id = UserSerializer()
+    # collaborating_group_id = GroupTagSerializer()
+    # collaborating_member_id = UserSerializer()
+
     class Meta:
         model = Schedules
         fields = ('title', 'start_time', 'end_time', 'is_all_day', 'notifying_time', 'collaborating_member_id', 'collaborating_group_id', 'memo', 'user_id')
        
 class FriendSerializer(serializers.ModelSerializer):
+    # user_id = UserSerializer()
+    # friend_user_id = UserSerializer()
+
     class Meta:
         model = Friends
         fields = ('user_id', 'friend_user_id')
 
 class AssignmentSerializer(serializers.ModelSerializer):
+    # user_id = UserSerializer()
+    # collaborating_group_id = GroupTagSerializer()
+    # collaborating_member_id = UserSerializer()
+
     class Meta:
         model = Assignments
         fields = ('name', 'start_time', 'end_time', 'is_finished', 'complete_time', 'required_time', 'notifying_time', 'collaborating_member_id', 'collaborating_group_id', 'memo', 'user_id')
 
 class TimeTableTimeSerializer(serializers.ModelSerializer):
+    # user_id = UserSerializer()
+    
     class Meta:
         model = TimeTableTimes
         fields = ('user_id', 'start_time', 'class_time', 'break_time', 'lunch_break_start_time', 'lunch_break_end_time')
 
-class GroupTagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = GroupTags
-        fields = ('name', 'user_id')
-
 class TimeTableSerializer(serializers.ModelSerializer):
+    # user_id = UserSerializer()
+
     class Meta:
         model = TimeTables
         fields = ('monday_timetable', 'tuesday_timetable', 'wednesday_timetable', 'thursday_timetable', 'friday_timetable', 'saturday_timetable', 'sunday_timetable', 'user_id')
 
 class ToDoListSerializer(serializers.ModelSerializer):
+    # user_id = UserSerializer()
+    # subjects_id = SubjectSerializer()
+
     class Meta:
         model = ToDoLists
         fields = ('name', 'subjects_id', 'limited_time', 'estimated_work_time', 'notifying_time', 'collaborating_member_id', 'collaborating_group_id', 'memo', 'is_work_finished', 'user_id')
 
 class ToDoListTaskSerializer(serializers.ModelSerializer):
+    # to_do_list_id = ToDoListSerializer()
+
     class Meta:
         model = ToDoListTasks
         fields = ('to_do_list_id', 'name', 'is_work_finished')
-
-class SubjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Subjects
-        fields = ('name', 'is_hidden', 'user_id', 'color_id')
 
 class ColorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Colors
         fields = ('name', 'red', 'green', 'blue')
+
+class SubjectSerializer(serializers.ModelSerializer):
+    # user_id = UserSerializer()
+    # color_id = ColorSerializer()
+
+    class Meta:
+        model = Subjects
+        fields = ('name', 'is_hidden', 'user_id', 'color_id')
+
