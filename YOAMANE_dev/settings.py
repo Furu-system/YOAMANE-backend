368948d10d98303b6b3792ea3264eb0d2e2963a5
Furu-system <mi7206furu@g.kumamoto-nct.ac.jp>
2021-09-21 12:10:33 +0000

    load env file

--- YOAMANE_dev/settings.py
+++ YOAMANE_dev/settings.py
@@ -12,19 +12,24 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR,'YOAMANE_dev/.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w0z=*36#!ui=dqk$#4)q4!1vu_=&(3k-^d_6gk(j0mijhcg7$g'
+# SECRET_KEY = 'django-insecure-w0z=*36#!ui=dqk$#4)q4!1vu_=&(3k-^d_6gk(j0mijhcg7$g'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ["sysken8.japanwest.cloudapp.azure.com"]
 
@@ -79,15 +84,12 @@ WSGI_APPLICATION = 'YOAMANE_dev.wsgi.application'
 
 DATABASES = {
     'default': {
-       # 'ENGINE': 'django.db.backends.sqlite3',
-       # 'NAME': BASE_DIR / 'db.sqlite3',
-         'ENGINE': 'django.db.backends.mysql',
-         'NAME': 'YOAMANE_dev',
-         'USER': 'sysken',
-         'PASSWORD': 'Sysken8246',
-         'HOST': '127.0.0.1',
-         'PORT': '3306',
-         
+         'ENGINE': env('SQL_ENGINE'),
+         'NAME': env('SQL_DATABASE'),
+         'USER': env('SQL_USER'),
+         'PASSWORD': env('SQL_PASSWORD'),
+         'HOST': env('SQL_HOST'),
+         'PORT': env('SQL_PORT'),
     }
 }
 
